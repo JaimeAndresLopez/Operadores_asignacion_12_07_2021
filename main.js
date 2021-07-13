9e855fb7b68346c65fbbbbd03d8f448ec15553fa
JaimeAndresLopez <james_2005_scott@hotmail.coml>
2021-07-12 21:12:24 -0500

    Operadores de asignacion y aritmeticos 12 07 2021

---
+++ main.js
@@ -0,0 +1,46 @@
+// let nombre;
+// nombre = `Jaime Andres Lopez Chacon`;
+// console.log(nombre);
+// let x;
+// let y = prompt(`Ingrese su edad`);
+// x = parseInt(y);/*transforma un string a int*/
+// console.log(x, typeof(x));
+// Operador de asignacion
+// x = y; le estoy asignando a X el vayor de Y
+// Operacion de adicion
+// Tener en cuenta que el  + en JS tambien se usa para concatenar
+// let x = 5;
+// let y = 1;
+// console.log(`Valor x inicializada ${x}`);
+// console.log(`Valor y inicializada ${y}`);
+// x += y;
+// x = x + y;
+// console.log(`Valor x operador de adicion ${x}`);
+// console.log(`Valor y operador de asicion ${y}`);
+// Operador de sustraccion
+// NaN significa q no es un numero
+// let x = `10`;
+// let y = 5;
+// x-=y;
+// console.log(x);
+// No importar q uno sea string y el otro number
+// Operador de multiplicacion
+// let x = 2;
+// let y = 4;
+// x *= y;
+// console.log(x);
+// Operadores de division
+// let x = 10;
+// let y = 2;
+// x /= y;
+// console.log(x);
+// Operador de resto o modulo
+// let x = 5;
+// let y = 3;
+// x %= y;
+// console.log(x);
+// Operador de exponenciacion
+let x = '2';
+let y = '3';
+x **= y;
+console.log(x, typeof(x)); /*El tipo sigue siendo de tipo int*/
